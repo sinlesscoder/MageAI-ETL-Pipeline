326fc973b6ca7780749004907f84bec49ff2ee22
sinlesscoder <aa6198@nyu.edu>
2023-06-23 19:48:27 +0000

    Update

--- src/frameworks/streamlit/practice.py
+++ src/frameworks/streamlit/practice.py
@@ -12,7 +12,7 @@ st.markdown("<h1 style='text-align:center'>Social Profile App </h1>", unsafe_all
 for i in range(len(data)):
     for j in range(len(data[i])):
         if j == 0:
-            st.markdown("<h3>Username: </h3>", unsafe_allow_html=True)
+            st.markdown("<h3>Name: </h3>", unsafe_allow_html=True)
             user_name = st.markdown(data[i][j], unsafe_allow_html=True)
         if j == 1:
             st.markdown("<h3>Age: </h3>", unsafe_allow_html=True)
@@ -20,6 +20,8 @@ for i in range(len(data)):
         if j == 2:
             st.markdown("<h3>Area: </h3>", unsafe_allow_html=True)
             area = st.markdown(data[i][j], unsafe_allow_html=True)
-    st.markdown("<h3>----------------------------------</h3>", unsafe_allow_html=True)
+    st.write("---")   
+    st.write("##")
+
 
 
