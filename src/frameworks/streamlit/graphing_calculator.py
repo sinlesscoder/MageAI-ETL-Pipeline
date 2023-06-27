7ece4ee19e866ba44ef55c8a68c16bcb21519e17
TheChanRProject <garpchoo@gmail.com>
2023-06-26 21:12:57 -0700

    Adding modified graphing calculator app.

--- src/frameworks/streamlit/graphing_calculator.py
+++ src/frameworks/streamlit/graphing_calculator.py
@@ -11,20 +11,27 @@ st.markdown("<h1 style='text-align:center'> Graphing Calculator </h1>", unsafe_a
 # Create a text area that has an equation
 equation = st.text_area("Write in an equation with respect to x: ", placeholder='x**2')
 
+# Numerical input
+start = st.number_input("Number to start the domain with: ", min_value=-1000000000, max_value=1000000000, value=-10)
+end = st.number_input("Number to end the domain with: ", min_value=-1000000000, max_value=1000000000, value=10)
+
+# Create a color picker
+graph_color = st.color_picker(label="Choose a color for the graph: ")
+
 # Graph function
-def graph(equation: str):
+def graph(equation: str, start, end, color):
     # Vector called x
-    x = np.linspace(-10, 10, num=200)
+    x = np.linspace(start, end, num=200)
 
     # Create the y vector
     y = eval(equation)
 
     # Matplotlib visual
     plt.figure(figsize=(10,10))
-
-    plt.plot(x, y)
+    plt.text(0, 2.0, f"y={equation}", size=16)
+    plt.plot(x, y, color=color)
 
     st.pyplot()
 
 if st.button("Graph"):
-    graph(equation)
+    graph(equation, start, end, graph_color)
