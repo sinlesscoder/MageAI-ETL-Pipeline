f1dce67a094347b6b20b9ff5bc40001846ec398d
TheChanRProject <garpchoo@gmail.com>
2023-06-27 20:19:06 -0700

    Adding part 1 to DuckDB and some initial caching.

---
+++ docs/frameworks/in_memory_databases/DuckDB_Intro.md
@@ -0,0 +1,32 @@
+## Intro to DuckDB
+
+### Description
+
+- `duckdb` is a new transaction-processing database that exists as a file in-memory. It's actually faster than the majority of relational databases in existence including PostgreSQL and Microsoft SQL.
+
+### Installation
+
+- `duckdb` can be installed using `pip`
+
+```bash
+pip install duckdb
+```
+
+- You can also access `duckdb` even faster by using the `duckdb_engine` which is compatible with `sqlalchemy`.
+
+```bash
+pip install duckdb_engine
+```
+
+### Basic Usage
+
+```python
+import duckdb
+import duckdb_engine
+from sqlalchemy import create_engine
+
+# Setup a connection object
+connection = duckdb.connect('example.db')
+
+#
+```
--- src/frameworks/streamlit/practice_optimized.py
+++ src/frameworks/streamlit/practice_optimized.py
@@ -1,5 +1,10 @@
+import duckdb
+import duckdb_engine
+import pandas as pd
 import streamlit as st
 from os import getcwd
+from sqlalchemy import create_engine
+
 
 # Create the data object as a function
 def create_object(name, age, area, image):
@@ -12,11 +17,51 @@ def create_object(name, age, area, image):
 
     return result
 
+# Convert data list to pandas DataFrame
+def create_df_from_objects(data_list):
+    # Get the dictionary keys
+    keys = list(data_list[0].keys())
+
+    # Create an empty dictionary
+    result_dict = {key : [] for key in keys}
+
+    for obj in data_list:
+        for key in keys:
+            # Transform into dictionary with lists of similar keys in each
+            result_dict[key].append(obj[key])
+    
+    # Convert to pandas DataFrame
+    return pd.DataFrame.from_dict(result_dict)
+
+# DuckDB Object Creation
+def duckdb_connection(db_name: str):
+    # Create the database dialect
+    dialect = f'duckdb:///{db_name}'
+
+    # Create connection to SQLAlchemy
+    engine = create_engine(dialect)
+
+    # Connection object
+    con = engine.connect()
+
+    return con
+
 data = [
     create_object('John', 25, 'New York', f'{getcwd()}/new-york-new-york-city.gif'),
     create_object('Emma', 28, 'London', f'{getcwd()}/fb95eca49cf3f2af57455b1358f1ec39193ce256.webp'),
     create_object('Tom', 32, 'Paris', f'{getcwd()}/france-eiffel-tower-paris.jpg')
 ]
+
+# Function to turn DataFrame into SQL table in duckDB
+def create_duckdb_sql_table(data):
+    # Upload the data to pandas DataFrame
+    result_df = create_df_from_objects(data)
+
+    # Create duckdb connection
+    duckdb_con = duckdb_connection('results.db')
+
+    # Export the DataFrame to a DuckDB table
+    result_df.to_sql('results', con=duckdb_con, index=False, if_exists='append')
 
 st.markdown("<h1 style='text-align:center'>Social Profile App </h1>", unsafe_allow_html=True)
 
@@ -37,6 +82,8 @@ if st.button('Upload'):
     # Add to the data list
     new_result = create_object(user_name, user_age, user_area, uploaded_image)
     data.append(new_result)
+    # Add to the DuckDB SQL table
+    create_duckdb_sql_table(data)
 
 # Loop through each data object
 for obj in data:
