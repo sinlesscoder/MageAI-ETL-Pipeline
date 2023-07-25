4ae585fb726d02c695c0aaa93d96ac74bb67799b
TheChanRProject <garpchoo@gmail.com>
2023-07-24 18:12:46 -0700

    Adding TOML markdown document.

---
+++ src/python_training/toml_example.py
@@ -0,0 +1,5 @@
+import toml
+
+data = toml.load("/Users/rchatter/Dropbox/My Mac (Rishovs-MacBook-Pro.local)/Desktop/Pathrise/Ali/Pathrise_Ali_Data_Tutorials/src/python_training/example.toml")
+
+print(data)
