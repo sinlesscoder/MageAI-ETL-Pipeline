9a30c557e8f50606dfe3ea44598b12aab3d1ea08
Ali Ahmed <hxhhisoka0@gmail.com>
2023-08-11 03:27:06 +0000

    Update

---
+++ src/python_training/pathrise_analytics/extract.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+from postgres_connection import engine
+from os import getcwd
+
+# Connection Object
+con = engine.connect()
+
+# Read the aliexpress_results table
+df = pd.read_sql_table('api_results', con=con)
+
+# View first 5 rows
+print(df.head())
+
+# Creating a ProfileReport object
+report = ProfileReport(df, dark_mode=True)
+
+# Report path
+report_path = getcwd() + "/docs/frameworks/profiling/profiles"
+
+# Save as HTML
+report.to_file(f'{report_path}/aliexpress_results_summary.html')
